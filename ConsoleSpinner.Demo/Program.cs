9f51b59c96051fd23863a45e947c9dd50c0002a1
Adrian Banks <github@adrianbanks.co.uk>
2021-08-23 18:57:05 +0100

    Simplify the demo code

--- ConsoleSpinner.Demo/Program.cs
+++ ConsoleSpinner.Demo/Program.cs
@@ -8,37 +8,19 @@ namespace ConsoleSpinner.Demo
     {
         internal static async Task Main()
         {
-            var cycleDuration = 100;
-
             foreach (SpinnerType spinnerType in Enum.GetValues(typeof(SpinnerType)))
             {
-                // DoIt(spinnerType, cycleDuration);
-                await DoItAsync(spinnerType, cycleDuration);
+                var spinner = new ConsoleSpinner(spinnerType);
+
+                var tokenSource = new CancellationTokenSource();
+                tokenSource.CancelAfter(TimeSpan.FromSeconds(6));
+
+                Console.Write($"{spinnerType} ");
+                // spinner.Write(tokenSource.Token);
+                await spinner.WriteAsync(tokenSource.Token);
 
                 Console.WriteLine();
             }
-        }
-
-        private static void DoIt(SpinnerType spinnerType, int cycleDuration)
-        {
-            var spinner = new ConsoleSpinner(spinnerType, cycleDuration);
-
-            var tokenSource = new CancellationTokenSource();
-            tokenSource.CancelAfter(TimeSpan.FromSeconds(6));
-
-            Console.Write($"{spinnerType} ");
-            spinner.Write(tokenSource.Token);
-        }
-
-        private static async Task DoItAsync(SpinnerType spinnerType, int cycleDuration)
-        {
-            var spinner = new ConsoleSpinner(spinnerType, cycleDuration);
-
-            var tokenSource = new CancellationTokenSource();
-            tokenSource.CancelAfter(TimeSpan.FromSeconds(4));
-
-            Console.Write($"{spinnerType} ");
-            await spinner.WriteAsync(tokenSource.Token);
         }
     }
 }
